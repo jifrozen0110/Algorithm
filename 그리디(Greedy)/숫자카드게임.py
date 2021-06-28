10015776426573b687e831241335b0404834eb50
jifrozen <answldjs1836@naver.com>
2021-06-28 22:12:46 +0900

    Fix 숫자카드게임.py

--- 그리디(Greedy)/숫자카드게임.py
+++ 그리디(Greedy)/숫자카드게임.py
@@ -1,9 +1,6 @@
-n,m=map(int,input().split())
-result=0
+n, m = map(int, input().split())
+result = []
 for i in range(n):
-    data=map(int,input().split())
-    min_value=min(data)
-    result=max(result,min_value)
+    result.append(min(list(map(int, input().split()))))
 
-print(result)
-
+print(max(result))
