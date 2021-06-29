75db1878dedfcc78036e8e1bba84d3559c2478c9
jifrozen <answldjs1836@naver.com>
2021-06-29 21:50:41 +0900

    Fix 1439 - 문자열 뒤집기.py

--- 그리디(Greedy)/문자열뒤집기.py
+++ 그리디(Greedy)/문자열뒤집기.py
@@ -1,20 +1,11 @@
-data=input()
+from math import ceil
 
-count0=0
-count1=0
+data = list(map(int, input()))
+m = data[0]
+result = 0
+for i in data:
+    if i != m:
+        m = i
+        result += 1
 
-if data[0]=='1':
-    count0+=1
-else:
-    count1+=1
-
-for i in range(len(data)-1):
-    if data[i]!=data[i+1]:
-        if data[i+1]=='1':
-            count0+=1
-        else:
-            count1+=1
-
-
-print(min(count1,count0))
-
+print(ceil(result / 2))
