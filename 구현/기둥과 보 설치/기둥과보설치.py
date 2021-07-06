17958a83dbfbe23e4faa57838fe0f8be1ff08d9d
jifrozen <answldjs1836@naver.com>
2021-07-07 01:56:46 +0900

    Fix 기둥과 보 설치

--- 구현/기둥과 보 설치/기둥과보설치.py
+++ 구현/기둥과 보 설치/기둥과보설치.py
@@ -1,37 +1,36 @@
-# https://programmers.co.kr/learn/courses/30/lessons/60061
+def possible(result):
+    for x, y, a in result:
+        if a == 0:
+            if y == 0 or [x - 1, y, 1] in result or [x, y, 1] in result or [x, y - 1, 0] in result:
+                continue
+            else:
+                return False
+        else:
+            if [x, y - 1, 0] in result or [x + 1, y - 1, 0] in result or (
+                    [x - 1, y, 1] in result and [x + 1, y, 1] in result):
+                continue
+            else:
+                return False
+    return True
 
-
-def possible(answer):
-     for x,y,stuff in answer:
-         if stuff==0:
-             if y==0 or [x-1,y,1] in answer or [x,y,1] in answer or [x,y-1,0] in answer:
-                 continue
-             else:
-                 return False
-         elif stuff==1:
-             if [x,y-1,0] in answer or [x+1,y-1,0] in answer or ([x-1,y,1] in answer and [x+1,y,1] in answer):
-                 continue
-             else:
-                 return False
-     return True
 
 def solution(n, build_frame):
-    answer = []
-    for frame in build_frame:
-        x,y,stuff,operate=frame
-        if operate==0:
-            answer.remove([x,y,stuff])
-            if not possible(answer):
-                answer.append([x,y,stuff])
+    result = []
+    for x, y, a, b in build_frame:
+        if b == 0:
+            result.remove([x, y, a])
+            if not possible(result):
+                result.append([x, y, a])
         else:
-            answer.append([x,y,stuff])
-            if not possible(answer):
-                answer.remove([x,y,stuff])
-    return sorted(answer)
+            result.append([x, y, a])
+            if not possible(result):
+                result.remove([x, y, a])
+    return sorted(result)
 
 
 print(solution
       (5,
-      [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
-      )
+       [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1], [2, 0, 0, 0],
+        [1, 1, 1, 0], [2, 2, 0, 1]]
+       )
       )
