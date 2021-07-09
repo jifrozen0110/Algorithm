36ff513fe0773c27e4ec0a9c391a262bff4358e9
jifrozen <answldjs1836@naver.com>
2021-07-09 12:32:01 +0900

    Create 10773.py - 제로

---
+++ 구현/10773.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/10773
+
+n = int(input())
+data = []
+for i in range(n):
+    a = int(input())
+    if a == 0:
+        data.pop()
+    else:
+        data.append(a)
+
+print(sum(data))
