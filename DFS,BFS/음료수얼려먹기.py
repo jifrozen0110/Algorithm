4206c8d8d70597efcbeb40bd427d6ae08320be27
jifrozen <answldjs1836@naver.com>
2021-07-14 22:32:20 +0900

    Fix 음료수 얼려먹기.py

--- DFS,BFS/음료수얼려먹기.py
+++ DFS,BFS/음료수얼려먹기.py
@@ -1,26 +1,30 @@
-n,m=map(int,input().split())
-data=[]
-for i in range (n):
-    data.append(list(map(int,input())))
+n, m = map(int, input().split())
+data = []
+for i in range(n):
+    data.append(list(map(int, input())))
 
-def dfs(x,y):
-    if x<=-1 or x>=n or y<=-1 or y>=m:
+
+def dfs(x, y):
+    # 밖으로 나가는 경우
+    if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
-    if data[x][y]==0:
-        data[x][y]=1
-        dfs(x-1,y)
-        dfs(x+1,y)
-        dfs(x,y-1)
-        dfs(x,y+1)
+    # 0이면 1로 방문 처리하고 상하좌우 재귀적으로 살펴봄
+    if data[x][y] == 0:
+        data[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
         return True
     return False
 
-result=0
+
+result = 0
+# 모든 노드 확인
 for i in range(n):
     for j in range(m):
-        if dfs(i,j)==True:
-            result+=1
-
-
+        # 재귀적으로 살피면서 0 (1)-> 0 (1)-> 0 (1)-> 1 return True
+        if dfs(i, j) == True:
+            result += 1
 
 print(result)
