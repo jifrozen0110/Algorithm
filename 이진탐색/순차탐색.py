cb0e14058356ee052676fac358b84d34186399dc
jifrozen <answldjs1836@naver.com>
2021-07-27 16:34:17 +0900

    Create - 이진/순차탐색

---
+++ 이진탐색/순차탐색.py
@@ -0,0 +1,16 @@
+# 순차탐색
+
+def sequential_search(n,target,array):
+    for i in range (n):
+        if array[i]==target:
+            return i+1
+
+print("생성할 원소 개수 입력 한 칸 띄고 찾을 문자열")
+input_data=input().split()
+n=int(input_data[0])
+target=input_data[1]
+
+print("앞서 적은 원소 개수만큼 문자열 입력")
+array=input().split()
+
+print(sequential_search(n,target,array))
