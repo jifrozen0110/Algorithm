3de31e4867473607fe891e62e660b1ff7662075d
jifrozen <answldjs1836@naver.com>
2021-09-08 16:30:34 +0900

    Review 구현 - 상하좌우, 왕실의나이트

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -1,18 +1,16 @@
-n = int(input())
+n=int(input())
+data=list(input().split())
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+dr=['L','R','U','D']
 
-data = list(input().split())
-
-dx = [0, -1, 0, 1]
-dy = [1, 0, -1, 0]
-dr = ['R', 'U', 'L', 'D']
-
-x, y = 0, 0
+x,y=1,1
 
 for i in data:
-    for j in range(len(dr)):
-        if i == dr[j]:
-            if x + dx[j] >= 0 and x + dx[j] < 5 and y + dy[j] >= 0 and y + dy[j] < 5:
-                x += dx[j]
-                y += dy[j]
+    for j in range (4):
+        if i==dr[j]:
+            if 5>=x+dx[j]>0 and 5>=y+dy[j]>0:
+                x=x+dx[j]
+                y=y+dy[j]
 
-print(x + 1, y + 1)
+print(y,x)
--- 구현/왕실의나이트.py
+++ 구현/왕실의나이트.py
@@ -1,14 +1,10 @@
-n = input()
-row = int(n[1])
-column = int(ord(n[0])) - int(ord('a')) + 1
-data = [(2, -1), (2, 1), (-2, 1), (-2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)]
-
-nx, ny = 0, 0
-result = 0
-for i in data:
-    nx = row + i[0]
-    ny = column + i[1]
-    if nx >= 1 and nx < 9 and ny >= 1 and ny < 9:
-        result += 1
+input_data=input()
+row=int(input_data[1])
+column=int(ord(input_data[0]))-int(ord('a'))+1
+case=[(2,-1),(2,1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+result=0
+for c in case:
+    if 8>=row+c[0]>0 and 8>=column+c[1]>0:
+        result+=1
 
 print(result)
