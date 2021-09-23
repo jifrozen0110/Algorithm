55664ee90e32826111c45d3c0d4e9c7b45c7dd49
jifrozen <answldjs1836@naver.com>
2021-09-24 01:55:09 +0900

    Create 문자열 charAt / StringBuilder reverse / toUpperCase / indexOf / substring

---
+++ 문자열/문장속단어.java
@@ -0,0 +1,34 @@
+import java.util.*;;
+
+public class Main {
+    public static String solution(String str) {
+        String answer = "";
+        int m = Integer.MIN_VALUE;// pos
+        /**
+         * while((pos=str.indexOf(' '))!=-1 )
+         *
+         * String temp=str.substring(0,pos); -> It/ is/ time ...
+         *
+         * str=str.substring(pos+1) 띄어쓰기 제외 그 다음 단어 얻기 위해
+         *
+         * 마지막 단어 따로 조건문으로 검사
+         */
+        String[] s = str.split(" ");
+        for (String x : s) {
+            int len = x.length();
+            if (len > m) {
+                m = len;
+                answer = x;
+
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(solution(str));
+    }
+
+}
---
+++ 문자열/중복문자제거.java
@@ -0,0 +1,21 @@
+import java.util.*;;
+
+public class Main {
+    public static String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i == str.indexOf(str.charAt(i))) {
+                answer += str.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+
+    }
+
+}
