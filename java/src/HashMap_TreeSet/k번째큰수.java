a8269b584d3354ababae29a583f0c473dec39c0d
jifrozen <answldjs1836@naver.com>
2021-10-24 19:15:42 +0900

    [Code] TreeSet

---
+++ java/src/HashMap_TreeSet/k번째큰수.java
@@ -0,0 +1,37 @@
+package HashMap_TreeSet;
+import java.util.*;
+import java.io.*;
+
+public class k번째큰수 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+
+		int[] arr=new int[n];
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++){
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+
+		TreeSet<Integer> Tset=new TreeSet<>(Collections.reverseOrder());
+
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				for(int l=j+1;l<n;l++){
+					Tset.add(arr[i]+arr[j]+arr[l]);
+				}
+			}
+		}
+		int cnt=0;
+		int answer=-1;
+		for(int x:Tset){
+			cnt++;
+			if(cnt==k) {answer=x; break;}
+		}
+
+		System.out.print(answer);
+	}
+}
