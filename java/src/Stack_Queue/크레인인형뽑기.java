8d4bb159342d7f6669cb512f567d1e4b143d7f4d
jifrozen <answldjs1836@naver.com>
2021-12-21 22:17:50 +0900

    [Code] prg 크레인_인형뽑기.java

--- java/src/Stack_Queue/크레인인형뽑기.java
+++ java/src/Stack_Queue/크레인인형뽑기.java
@@ -3,45 +3,28 @@ import java.util.*;
 import java.io.*;
 
 public class 크레인인형뽑기 {
-	public static void main(String[] agrs) throws IOException{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		int[][] arr=new int[n][n];
-		for(int i=0;i<n;i++){
-			st=new StringTokenizer(br.readLine());
-			for(int j=0;j<n;j++){
-				arr[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-		int m=Integer.parseInt(br.readLine());
-		int[] moves=new int[m];
-		st=new StringTokenizer(br.readLine());
-		for(int i=0;i<m;i++){
-			moves[i]=Integer.parseInt(st.nextToken());
-		}
+	class Solution {
+		public int solution(int[][] board, int[] moves) {
+			int answer = 0;
 
-		Stack<Integer> stack=new Stack<>();
-		stack.add(0);
-		int answer=0;
-		for(int i=0;i<m;i++){
-			int num=moves[i];
-			for(int j=0;j<n;j++){
-				if(arr[j][num-1]!=0){
-					if(arr[j][num-1]==stack.peek()){
-						stack.pop();
-						answer+=2;
-					}else {
-						stack.push(arr[j][num - 1]);
+			Stack<Integer> stack = new Stack<>();
+			stack.push(0);
+
+			for (int move : moves) {
+				for (int j = 0; j < board.length; j++) {
+					if (board[j][move - 1] != 0) {
+						if (stack.peek() == board[j][move - 1]) {
+							stack.pop();
+							answer += 2;
+						} else {
+							stack.push(board[j][move - 1]);
+						}
+						board[j][move - 1] = 0;
+						break;
 					}
-					arr[j][num-1]=0;
-					break;
 				}
 			}
-
+			return answer;
 		}
-
-		System.out.print(answer);
-
 	}
 }
