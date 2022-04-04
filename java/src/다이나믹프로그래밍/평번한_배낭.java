3a6dea93e5a630fa0372774f5cf0d609efef0caf
jifrozen <answldjs1836@naver.com>
2022-04-04 20:29:47 +0900

    Code : boj 12865.java - 평범한 배낭

---
+++ java/src/다이나믹프로그래밍/평번한_배낭.java
@@ -0,0 +1,43 @@
+package 다이나믹프로그래밍;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 평번한_배낭 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+
+		int[][] item=new int[n+1][2]; // 아이템 갯수만큼 아이템 번호 / 아이템 가치
+
+		for(int i=1;i<=n;i++){
+			st=new StringTokenizer(br.readLine());
+			item[i][0]=Integer.parseInt(st.nextToken());
+			item[i][1]=Integer.parseInt(st.nextToken());
+		}
+
+		int[][] dp=new int[n+1][k+1];
+
+		for(int i=1;i<=k;i++){
+			for(int j=1;j<=n;j++){
+				dp[j][i]=dp[j-1][i];
+				if(i-item[j][0]>=0){
+					dp[j][i]=Math.max(dp[j-1][i],item[j][1]+dp[j-1][i-item[j][0]]);
+				}
+			}
+		}
+
+		System.out.println(dp[n][k]);
+
+
+
+
+
+
+	}
+}
