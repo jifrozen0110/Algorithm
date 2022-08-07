075ea79192f97b2254cdaf55eca9d9e2389d2b91
jifrozen <answldjs1836@naver.com>
2022-08-07 18:03:28 +0900

    Code : 이코테 다이나믹프로그래밍

---
+++ java/src/다이나믹프로그래밍/일로_만들기_2.java
@@ -0,0 +1,30 @@
+package 다이나믹프로그래밍;
+
+import java.util.Scanner;
+
+public class 일로_만들기_2 {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int[] d=new int[30001];
+		int X=sc.nextInt();
+
+		for(int i=2;i<=X;i++){
+			d[i]=d[i-1]+1;
+			if(i%2==0){
+				d[i]=Math.min(d[i],d[i/2]+1);
+			}
+			if(i%3==0){
+				d[i]=Math.min(d[i],d[i/3]+1);
+			}
+			if(i%5==0){
+				d[i]=Math.min(d[i],d[i/5]+1);
+			}if(i%3==0){
+				d[i]=Math.min(d[i],d[i/2]+1);
+			}
+		}
+
+		System.out.println(d[X]);
+
+
+	}
+}
