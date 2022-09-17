3590a3bb21cbbe3bcdf56e9a822b0f9fcf04cdaa
jifrozen <answldjs1836@naver.com>
2022-09-18 03:26:09 +0900

    Code : Boj2018

--- java/src/Two_pointer/수들의_합_5.java
+++ java/src/Two_pointer/수들의_합_5.java
@@ -4,26 +4,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class 수들의_합_5 {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int N=Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-		long start=1;
-		long end=1;
+		long start = 1;
+		long end = 1;
 
-		long sum=1;
-		long answer=0;
-		while(end<=N){
-			if(sum<N){
+		long sum = 1;
+		long answer = 0;
+		while (end <= N) {
+			if (sum < N) {
 				end++;
-				sum+=end;
-			}else if(sum>N){
-				sum-=start;
+				sum += end;
+			} else if (sum > N) {
+				sum -= start;
 				start++;
-			}else{
+			} else {
 				end++;
 				answer++;
-				sum+=end;
+				sum += end;
 			}
 		}
 
