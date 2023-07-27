2b4864c3045e525b5cfa775c13eeb82add5168aa
jifrozen0110 <answldjs1836@gmail.com>
2023-07-27 23:05:39 +0900

    code : 숫자

---
+++ java/src/숫자/나머지.java
@@ -0,0 +1,24 @@
+package 숫자;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class 나머지 {
+
+		public static void main(String[] args) throws IOException {
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String[] str = br.readLine().split(" ");
+			int A = Integer.parseInt(str[0]);
+			int B = Integer.parseInt(str[1]);
+			int C = Integer.parseInt(str[2]);
+
+			System.out.println( (A+B)%C );
+			System.out.println( (A%C + B%C)%C );
+			System.out.println( (A*B)%C );
+			System.out.println( (A%C * B%C)%C );
+
+		}
+
+	}
+}
