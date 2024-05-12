a92374a06273cd850bbac49fd620e6656de41dd4
jifrozen0110 <answldjs1836@gmail.com>
2024-05-12 13:22:10 +0900

    [level 2] Title: 방문 길이, Time: 13.51 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,6 +1,33 @@
+import java.util.*;
 class Solution {
+    static int[] dx={-1,1,0,0};
+    static int[] dy={0,0,1,-1};
+    static char[] dirArr={'U','D','R','L'};
     public int solution(String dirs) {
         int answer = 0;
-        return answer;
+        
+        int startX=5;
+        int startY=5;
+
+        HashSet<String> set=new HashSet<>();
+        
+        for(char dir:dirs.toCharArray()){
+            for(int c=0;c<4;c++){
+                if(dir==dirArr[c]){
+                    int nx=startX+dx[c];
+                    int ny=startY+dy[c];
+                    
+                    if(nx<0||ny<0||nx>=11||ny>=11) continue;
+                    
+                    set.add(startX+" "+startY+" "+nx+" "+ny);
+                    set.add(nx+" "+ny+" "+startX+" "+startY);
+                    
+                    startX=nx;
+                    startY=ny;
+                    
+                }
+            }
+        }
+        return set.size()/2;
     }
 }
