74d1af145aa232ca69f93decd4789eda39d369a1
jifrozen0110 <answldjs1836@gmail.com>
2024-05-13 11:12:39 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.46 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -3,28 +3,24 @@ class Solution
 {
     public int solution(String s)
     {
-        int answer = 0;
-        
-        if(s.length()%2==1) return answer;
-        
-        Stack<Character> stack=new Stack<>();
+        int answer = -1;
+        Deque<Character> stack=new ArrayDeque<>();
         
         for(char c:s.toCharArray()){
-            if(stack.size()==0){
-                stack.push(c);
+            if(stack.isEmpty()){
+                stack.addFirst(c);
+            }
+            else if(stack.peekFirst()==c){
+                stack.removeFirst();
             }else{
-                char prev=stack.pop();
-                if(prev!=c){
-                    stack.push(prev);
-                    stack.push(c);
-                }
+                stack.addFirst(c);
             }
         }
         
-        if(stack.size()==0){
-            answer=1;
+        if(!stack.isEmpty()){
+            return 0;
         }
         
-        return answer;
+        return 1;
     }
 }
