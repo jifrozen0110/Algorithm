20824eec2176dae6367ad49ea10fdebce5385af0
jifrozen0110 <answldjs1836@gmail.com>
2024-05-15 18:38:29 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,39 +1,41 @@
 import java.util.*;
 class Solution {
+    static HashMap<String,Integer> map=new HashMap<>();
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String,Integer> wantMap=new HashMap<>();
         
-        for(int i=0;i<want.length;i++){
-            wantMap.put(want[i],number[i]);
+        
+        
+        for(int i=0;i<9;i++){
+            map.put(discount[i],map.getOrDefault(discount[i],0)+1);
         }
         
-        HashMap<String,Integer> tempMap=new HashMap<>();
-        for(int i=0;i<10;i++){
-            tempMap.put(discount[i],tempMap.getOrDefault(discount[i],0)+1);
-        }
+        int index=0;
         
-        if (check(wantMap, tempMap)) answer++;
-        
-        for(int i=1;i<discount.length-9;i++){
-            String previousKey = discount[i - 1];
-            int end=i+9;
-            tempMap.put(previousKey,tempMap.get(previousKey)-1);
-            if (tempMap.get(previousKey) == 0) {
-            tempMap.remove(previousKey);
-        }
-            tempMap.put(discount[end],tempMap.getOrDefault(discount[end],0)+1);
-            
-        if (check(wantMap, tempMap)) answer++;
-            
+        for(int i=9;i<discount.length;i++){
+            map.put(discount[i],map.getOrDefault(discount[i],0)+1);
+            if(isDiscount(want,number)){
+                answer++;
+            }
+            map.put(discount[index],map.get(discount[index])-1);
+            index++;
         }
         
         return answer;
     }
-    public static boolean check(Map<String, Integer> target, Map<String, Integer> map) {
-        for (String key: map.keySet()) {
-            if (!target.containsKey(key) || map.get(key) != target.get(key)) return false;
+    
+    public boolean isDiscount(String[] want,int[] number){
+        
+        for(int i=0;i<want.length;i++){
+            if(map.containsKey(want[i])){
+                if(map.get(want[i])<number[i]){
+                    return false;
+                }
+            }else{
+                return false;
+            }
         }
+        
         return true;
     }
 }
