913353adc560a3298a7de9333fe920aa00a4b805
jifrozen0110 <answldjs1836@gmail.com>
2024-05-16 21:38:20 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 21.17 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,53 +1,71 @@
 import java.util.*;
 class Solution {
-    static HashMap<String,Integer> map=new HashMap<>();
-    public String[] solution(String[] orders, int[] course) {
+    static HashMap<String,Integer> courses;
+    class Course implements Comparable<Course>{
+        String name;
+        int count;
         
-        for(int i=0;i<orders.length;i++){
-            char[] charArray=orders[i].toCharArray();
-            Arrays.sort(charArray);
-            orders[i]=new String(charArray);
+        Course(String name,int count){
+            this.name=name;
+            this.count=count;
         }
         
-        for(String order:orders){
-            for(int c:course){
-                DFS(order,0,c,new StringBuilder());
+        @Override
+        public int compareTo(Course o){
+            if(this.count==o.count){
+                return this.name.compareTo(o.name);
             }
+            return o.count-this.count;
         }
-        ArrayList<String> arr=new ArrayList<>();
-       
+    }
+    public String[] solution(String[] orders, int[] course) {
+        ArrayList<String> answer=new ArrayList<>();
+        
         for(int c:course){
-            int maxCount=0;
-            for(String key:map.keySet()){
-                if(key.length()==c){
-                    maxCount=Math.max(maxCount,map.get(key));
+            ArrayList<Course> result=new ArrayList<>();
+            courses=new HashMap<>();
+            for(String order:orders){
+                char[] ordera=order.toCharArray();
+                Arrays.sort(ordera);
+                DFS(c,0,ordera,"");
+            }
+            int max=0;
+            for(String m:courses.keySet()){
+                if(courses.get(m)>=2){
+                    result.add(new Course(m,courses.get(m)));
+                    max=Math.max(max,courses.get(m));
                 }
             }
             
-            for(String key:map.keySet()){
-                if(key.length()==c&&map.get(key)==maxCount&&maxCount>1){
-                    arr.add(key);
+            if(result.size()==0){
+                continue;
+            }
+            
+            Collections.sort(result);
+            
+            for(int i=0;i<result.size();i++){
+                if(max==result.get(i).count){
+                    answer.add(result.get(i).name);
+                }else{
+                    break;
                 }
             }
+            
+          
         }
         
-        String[] answer=new String[arr.size()];
-        answer=arr.toArray(answer);
-        Arrays.sort(answer);
+        Collections.sort(answer);
         
-        return answer;
+        
+        return answer.toArray(new String[0]);
     }
-    public void DFS(String order,int index,int course,StringBuilder sb){
-        if(course==sb.length()){
-            String target=sb.toString();
-            map.put(target,map.getOrDefault(target,0)+1);
+    public void DFS(int count,int index,char[] arr,String result){
+        if(count==result.length()){
+            courses.put(result,courses.getOrDefault(result,0)+1);
+            return;
         }
-        
-        for(int i=index;i<order.length();i++){
-            sb.append(order.charAt(i));
-            DFS(order,i+1,course,sb);
-            sb.deleteCharAt(sb.length()-1);
-                
+        for(int i=index;i<arr.length;i++){
+            DFS(count,i+1,arr,result+arr[i]);
         }
     }
 }
