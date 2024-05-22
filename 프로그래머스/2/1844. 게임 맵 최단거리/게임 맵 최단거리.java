b395af06691922610818b850106624f1e288dc37
jifrozen0110 <answldjs1836@gmail.com>
2024-05-22 15:55:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.43 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,31 @@
 import java.util.*;
 class Solution {
-    static int[] dx={0,1,0,-1};
-    static int[] dy={-1,0,1,0};
+    static int[] dx={-1,0,1,0};
+    static int[] dy={0,1,0,-1};
     public int solution(int[][] maps) {
         int answer = 0;
         
-        int col=maps.length,row=maps[0].length;
-        int[][] visited=new int[col][row];
-        Queue<int[]> q=new LinkedList<>();
-        
-        int curX=0,curY=0;
-        visited[curX][curY]=1;
-        q.offer(new int[] {curX,curY});
+        ArrayDeque<int[]> q=new ArrayDeque<>();
+        q.add(new int[]{0,0});
+        int[][] visited=new int[maps.length][maps[0].length];
+        visited[0][0]=1;
         
         while(!q.isEmpty()){
-            int[] node=q.poll();
-            for(int i=0;i<4;i++){
-                int nextX=node[0]+dx[i];
-                int nextY=node[1]+dy[i];
-                if(nextX<0||nextY<0||nextX>=col||nextY>=row){
+            int[] current=q.poll();
+            for(int d=0;d<4;d++){
+                int nx=current[0]+dx[d];
+                int ny=current[1]+dy[d];
+                if(nx<0||ny<0||nx>=maps.length||ny>=maps[0].length){
                     continue;
                 }
-                if(visited[nextX][nextY]!=0) continue;
-                if(maps[nextX][nextY]==0) continue;
-                visited[nextX][nextY]=visited[node[0]][node[1]]+1;
-                q.offer(new int[] {nextX,nextY});
+                if(visited[nx][ny]!=0) continue;
+                if(maps[nx][ny]==0) continue;
+                q.add(new int[]{nx,ny});
+                visited[nx][ny]=visited[current[0]][current[1]]+1;
             }
         }
         
-        return (visited[col-1][row-1]==0)?-1:visited[col-1][row-1];
+        // return 0;
+        return visited[maps.length-1][maps[0].length-1]==0?-1:visited[maps.length-1][maps[0].length-1];
     }
 }
