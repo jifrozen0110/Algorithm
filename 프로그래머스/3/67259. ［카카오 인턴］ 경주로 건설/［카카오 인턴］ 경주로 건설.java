292403264d5fc4d5ae741f7b8c498dcc569142b0
jifrozen0110 <answldjs1836@gmail.com>
2024-05-24 15:12:34 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.32 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Solution {
+    static int[] dx={0,-1,0,1};//0 2
+    static int[] dy={-1,0,1,0};//1 3
+    static int INF=Integer.MAX_VALUE;
+    class Node{
+        int x;
+        int y;
+        int direction;
+        int cost;
+        public Node(int x,int y,int direction,int cost){
+            this.x=x;
+            this.y=y;
+            this.direction=direction;
+            this.cost=cost;
+        }
+    }
+    public int solution(int[][] board) {
+        int answer = INF;
+        int n=board.length;
+        int[][][] visited=new int[board.length][board.length][4];
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<n;j++){
+                Arrays.fill(visited[i][j],INF);
+            }
+        }
+        
+        ArrayDeque<Node> q=new ArrayDeque<>();
+        visited[0][0][0]=0;
+        visited[0][0][1]=0;
+        visited[0][0][2]=0;
+        visited[0][0][3]=0;
+        q.add(new Node(0,0,-1,0));
+        
+        while(!q.isEmpty()){
+            Node now=q.poll();
+            for(int d=0;d<4;d++){
+                int nx=now.x+dx[d];
+                int ny=now.y+dy[d];
+                 if (nx < 0 || ny < 0 || nx >= n || ny >= n || board[nx][ny] == 1) continue;
+
+                int newCost;
+                if(now.direction == -1||(now.direction-d)%2 == 0){
+                    newCost = now.cost + 100;
+                }else{
+                    newCost = now.cost + 600;
+                }
+                
+                if(nx==n-1&&ny==n-1){
+                    answer=Math.min(answer,newCost);
+                }
+
+                else if (visited[nx][ny][d]==0||visited[nx][ny][d] > newCost) {
+                    visited[nx][ny][d]= newCost;
+                    q.add(new Node(nx, ny, d % 2,newCost));
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
