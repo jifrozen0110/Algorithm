20a31a3b1920a0026128c30a6631525eef610494
jifrozen0110 <answldjs1836@gmail.com>
2024-06-02 17:39:53 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0;i<commands.length;i++){
+
+            int a=commands[i][0];
+            int b=commands[i][1];
+            int c=commands[i][2];
+            
+            int[] slices=Arrays.copyOfRange(array,a-1,b);
+            Arrays.sort(slices);
+            answer[i]=slices[c-1];
+        }
+        return answer;
+    }
+}
