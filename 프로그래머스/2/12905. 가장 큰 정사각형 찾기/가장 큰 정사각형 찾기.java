cf140effd6ce7a07a5d632848b3bba0f715400be
jifrozen0110 <answldjs1836@gmail.com>
2024-06-07 18:46:06 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 13.45 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -3,25 +3,24 @@ class Solution
     public int solution(int [][]board)
     {
         int answer = 0;
-
-        int row=board.length;
-        int col=board[0].length;
         
-        if(row<2||col<2){
-            return 1;
+        for(int i=0;i<board.length;i++){
+            answer=Math.max(answer,board[i][0]);
         }
         
-        for(int i=1;i<row;i++){
-            for(int j=1;j<col;j++){
-                if(board[i][j]==1){
-                    board[i][j]=Math.min(board[i-1][j-1],Math.min(board[i-1][j],board[i][j-1]))+1;
+        for(int i=0;i<board[0].length;i++){
+            answer=Math.max(answer,board[0][i]);
+        }
+        
+        for(int i=1;i<board.length;i++){
+            for(int j=1;j<board[0].length;j++){
+                if(board[i][j]>0){
+                    board[i][j]=Math.min(Math.min(board[i-1][j-1],board[i][j-1]),board[i-1][j])+1;
                 }
-                if(answer<board[i][j]){
-                    answer=board[i][j];
-                }
+                answer=Math.max(answer,board[i][j]);
             }
         }
-        
+
         return answer*answer;
     }
 }
