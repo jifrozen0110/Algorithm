f3d0896528580ae22bf1563eb9a050ab8c428ad4
jifrozen0110 <answldjs1836@gmail.com>
2024-06-09 01:15:20 +0900

    [level 2] Title: 호텔 대실, Time: 23.16 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,45 +1,44 @@
-import java.util.*;
-
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.PriorityQueue;
 
 class Solution {
-    class Time implements Comparable<Time>{
-        int startTime;
-        int endTime;
-        
-        public Time(int startTime, int endTime){
-            this.startTime = startTime;
-            this.endTime = endTime;
+    class Time implements Comparable<Time> {
+        LocalTime startTime;
+        LocalTime endTime;
+
+        public Time(String startTime, String endTime) {
+            DateTimeFormatter formatter=DateTimeFormatter.ofPattern("HH:mm");
+            this.startTime=LocalTime.parse(startTime,formatter);
+            this.endTime=LocalTime.parse(endTime,formatter);
         }
-        
+
         @Override
-        public int compareTo(Time o){
-            return this.startTime - o.startTime;
+        public int compareTo(Time o) {
+            return this.startTime.compareTo(o.startTime);
+
         }
     }
-    
+
     public int solution(String[][] book_time) {
         PriorityQueue<Time> pq = new PriorityQueue<>();
-        
-        for(String[] t : book_time){
-            String[] start = t[0].split(":");
-            String[] end = t[1].split(":");
-            int startTime = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
-            int endTime = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
-            pq.add(new Time(startTime, endTime));
+
+        for (String[] t : book_time) {
+            pq.add(new Time(t[0], t[1]));
         }
-        
-        PriorityQueue<Integer> room = new PriorityQueue<>();
-        
-        while(!pq.isEmpty()){
+
+        PriorityQueue<LocalTime> room = new PriorityQueue<>();
+
+        while (!pq.isEmpty()) {
             Time time = pq.poll();
-            if(!room.isEmpty() && room.peek() <= time.startTime){
+            if (!room.isEmpty() && room.peek().plusMinutes(10).compareTo(time.startTime) <= 0) {
                 room.poll();
             }
-            
-            room.add(time.endTime + 10);
+            room.add(time.endTime);
         }
-        
+
         return room.size();
     }
+
+
 }
-    
