75bab917c44e33e05590f7d48b3ca77a4e2ffc7f
jifrozen0110 <answldjs1836@gmail.com>
2024-06-11 17:40:45 +0900

    [level 2] Title: 귤 고르기, Time: 68.96 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,36 +1,50 @@
 import java.util.*;
 class Solution {
+    class Tangerine implements Comparable<Tangerine>{
+        int num;
+        int count;
+        
+        public Tangerine(int num, int count){
+            this.num=num;
+            this.count=count;
+        }
+        
+        @Override
+        public int compareTo(Tangerine o){
+            return o.count-this.count;
+        }
+    }
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         
         Arrays.sort(tangerine);
         
-        ArrayList<Integer> arr=new ArrayList<>();
-        
         int prev=tangerine[0];
         int count=0;
         
-        for(int tan:tangerine){
-            if(prev!=tan){
-                arr.add(count);
-                prev=tan;
-                count=1;
-            }else{
+        ArrayList<Tangerine> tangerines=new ArrayList<>();
+        for(int t:tangerine){
+            if(prev==t){
                 count++;
+            }else{
+                tangerines.add(new Tangerine(prev,count));
+                prev=t;
+                count=1;
             }
         }
-        arr.add(count);
+        tangerines.add(new Tangerine(prev,count));
         
-        Collections.sort(arr,Collections.reverseOrder());
         
-        for(int i=0;i<arr.size();i++){
-            if(k<=0){
+        Collections.sort(tangerines);
+        
+        for(int i=0;i<tangerines.size();i++){
+            if(k>0){
+                k-=tangerines.get(i).count;
+                answer++;
+            }else{
                 break;
             }
-            k-=arr.get(i);
-            answer++;
         }
-        
         return answer;
     }
 }
