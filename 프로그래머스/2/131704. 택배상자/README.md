6baf92c28d57024dafa4a22b83b8b6e72add3659
jifrozen0110 <answldjs1836@gmail.com>
2024-06-25 20:19:20 +0900

    [level 2] Title: 택배상자, Time: 137.92 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
 
 ### 성능 요약
 
-메모리: 170 MB, 시간: 173.62 ms
+메모리: 130 MB, 시간: 137.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 25일 20:06:45
+2024년 06월 25일 20:19:18
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,31 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(int[] order) {
-        int answer = 0;
-        
-        
-        Queue<Integer> q=new LinkedList<>();
-        for(int i=1;i<=order.length;i++){
-            q.add(i);
-        }
         
         Stack<Integer> stack=new Stack<>();
         
-        int cur=1;
+        int idx=0;
         for(int i=0;i<order.length;i++){
-            while(!q.isEmpty()&&q.peek()<order[i]){
-                stack.push(q.poll());
+            stack.push(i+1);
+            
+            while(!stack.isEmpty()){
+                if(stack.peek()==order[idx]){
+                    stack.pop();
+                    idx++;
+                }else break;
             }
-            if(!q.isEmpty()&&q.peek()==order[i]){
-                q.poll();
-                answer++;
-                continue;   
-            }
-            if(!stack.isEmpty()&&stack.pop()!=order[i]){
-                break;
-            }
-            answer++;
         }
-        return answer;
+        
+        return idx;
     }
 }
