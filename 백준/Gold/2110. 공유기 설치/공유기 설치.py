22356c271c0494c1eeb3b360a2475a1003227694
jifrozen0110 <answldjs1836@gmail.com>
2024-08-13 14:49:34 +0900

    [Gold IV] Title: 공유기 설치, Time: 544 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -6,20 +6,19 @@ wifis=[int(input()) for _ in range (N)]
 wifis.sort()
 
 left=1
-right=wifis[N-1]
+right=wifis[-1]-wifis[0]
 answer=0
 while left<=right:
     mid=(left+right)//2
 
     start=wifis[0]
     cnt=1
-    for i in range (1,len(wifis)):
-        dist=wifis[i]-start
-        if dist>=mid:
+    for i in range (1,N):
+        if wifis[i]-start>=mid:
             start=wifis[i]
             cnt+=1
     if cnt>=C:
-        answer=max(answer,mid)
+        answer=mid
         left=mid+1
     else:
         right=mid-1
