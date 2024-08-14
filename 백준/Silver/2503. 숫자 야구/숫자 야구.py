20a9e4c784169ae5f125e780e1e3e84ab45584ee
jifrozen0110 <answldjs1836@gmail.com>
2024-08-14 16:07:08 +0900

    [Silver III] Title: 숫자 야구, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,56 +1,45 @@
-# 4
-# 123 1 1
-# 356 1 0
-# 327 2 0
-# 489 0 1
-
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-N=int(input())
-games=[]
-for _ in range (N):
-    num_str,strike,ball=input().split()
-    num_list=list(map(int,num_str))
-    strike=int(strike)
-    ball=int(ball)
-    games.append((num_list,strike,ball))
+N = int(input())
+games = []
+for _ in range(N):
+    num_str, strike, ball = input().split()
+    num_list = list(map(int, num_str))
+    strike = int(strike)
+    ball = int(ball)
+    games.append((num_list, strike, ball))
 
-visited=[0]*10
-answer=0
-def start(result):
+answer = 0
+
+def checkNumber(original, target):
+    strike, ball = 0, 0
+    for i in range(3):
+        if original[i] == target[i]:
+            strike += 1
+        elif original[i] in target:
+            ball += 1
+    return strike, ball
+
+def isValid(number):
+    for compareNum, strike, ball in games:
+        s, b = checkNumber(number, compareNum)
+        if s != strike or b != ball:
+            return False
+    return True
+
+def makeNumber():
     global answer
-    for game in games:
-        compareNum=game[0]
-        strike=game[1]
-        ball=game[2]
-        s,b=checkNumber(result,compareNum)
-        if s!=strike or ball!=b:
-            return
-    answer+=1
+    for i in range(1, 10):
+        for j in range(1, 10):
+            if i == j:
+                continue
+            for k in range(1, 10):
+                if i == k or j == k:
+                    continue
+                candidate = [i, j, k]
+                if isValid(candidate):
+                    answer += 1
 
-
-def checkNumber(original,target):
-    strike=0
-    ball=0
-    for i in range (len(original)):
-        if original[i]==target[i]:
-            strike+=1
-            continue
-        if original[i] in target:
-            ball+=1
-    return strike,ball
-
-
-def makeNumber(result,cnt):
-    if cnt==3:
-        start(result)
-    for i in range(1,10):
-        if not visited[i]:
-            visited[i]=True
-            makeNumber(result+[i],cnt+1)
-            visited[i]=False
-
-
-makeNumber([],0)
+makeNumber()
 print(answer)
