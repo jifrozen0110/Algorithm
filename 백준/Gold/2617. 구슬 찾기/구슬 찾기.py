e0dc66fbfec8490652035ecbbf303a8471b7a5fb
jifrozen0110 <answldjs1836@gmail.com>
2024-08-20 15:14:32 +0900

    [Gold IV] Title: 구슬 찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -0,0 +1,35 @@
+import sys
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+
+balls=[[]*N for _ in range (N)]
+
+indegree=[0]*N
+small=[0]*N
+big=[0]*N
+indegree=[0]*N
+for i in range (M):
+    a,b=map(int,input().split())
+    balls[a-1].append(b-1)
+    indegree[b-1]+=1
+def DFS(start,cur):
+    visited[start]=True
+    for i in balls[start]:
+        if not visited[i]:
+            small[cur]+=1
+            big[i]+=1
+            DFS(i,cur)
+
+
+for i in range (N):
+    visited=[False]*N
+    DFS(i,i)
+
+
+half=N//2
+cnt=0
+for i in range (N):
+    if half<small[i] or half<big[i]:
+        cnt+=1
+print(cnt)
