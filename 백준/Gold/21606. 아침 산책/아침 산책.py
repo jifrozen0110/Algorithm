e074bae5b3546809572c82ea9ed1d3cd2fba5c1d
jifrozen0110 <answldjs1836@gmail.com>
2024-08-20 09:40:39 +0900

    [Gold III] Title: 아침 산책, Time: 836 ms, Memory: 102080 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,34 +1,37 @@
 import sys
-sys.setrecursionlimit(100000)
-input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+input=sys.stdin.readline
 
-N = int(input())
+N=int(input())
 
-arr = list(input().rstrip())
-visited = [False] * (N + 1)
-graph = [[] for _ in range(N + 1)]
-total = 0
-
-for i in range(N - 1):
-    u, v = map(int, input().split())
+arr="0"+input()
+visited=[False]*(N+1)
+graph=[[] for _ in range (N+1)]
+total=0
+for i in range (N-1):
+    u,v=map(int,input().split())
     graph[u].append(v)
     graph[v].append(u)
-    if arr[u - 1] == '1' and arr[v - 1] == '1':
-        total += 2
+    if arr[u]=='1' and arr[v]=='1':
+        total+=2
 
 def DFS(start):
-    visited[start] = True
-    inside = 0
+    visited[start]=True
+    inside=0
     for i in graph[start]:
-        if arr[i - 1] == '1':
-            inside += 1
-        elif not visited[i] and arr[i - 1] == '0':
-            inside += DFS(i)
+        if arr[i]=='1':
+            inside+=1
+        elif not visited[i] and arr[i]=='0':
+            inside+=DFS(i)
     return inside
 
-for i in range(1, N + 1):
-    if not visited[i] and arr[i - 1] == '0':
-        result = DFS(i)
-        total += result * (result - 1)
+
+for i in range (1,N+1):
+    if not visited[i] and arr[i]=='0':
+        result=DFS(i)
+        total+=(result)*(result-1)
 
 print(total)
+
+
+
