4ac6499242b60707b78be8fa464ac2db18c327b4
jifrozen0110 <answldjs1836@gmail.com>
2024-08-20 09:27:01 +0900

    [Gold III] Title: 아침 산책, Time: 504 ms, Memory: 67576 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -0,0 +1,34 @@
+import sys
+
+input=sys.stdin.readline
+
+N=int(input())
+
+arr=list(input().rstrip())
+visited=[False]*(N+1)
+graph=[[] for _ in range (N+1)]
+total=0
+for i in range (N-1):
+    u,v=map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    if arr[u-1]=='1' and arr[v-1]=='1':
+        total+=2
+
+def DFS(start):
+    visited[start]=True
+    inside=0
+    for i in graph[start]:
+        if arr[i-1]=='1':
+            inside+=1
+        elif not visited[i] and arr[i-1]=='0':
+            inside+=DFS(i)
+    return inside
+
+
+for i in range (1,N+1):
+    if not visited[i] and arr[i-1]=='1':
+        result=DFS(i)
+        total+=result*(result-1)
+
+print(total)
