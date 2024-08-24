c50ae6a1fb243949ba237e59ae6e14faee61e6db
jifrozen0110 <answldjs1836@gmail.com>
2024-08-24 21:23:36 +0900

    [Gold III] Title: 양팔저울, Time: 40 ms, Memory: 34396 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -1,40 +1,37 @@
 import sys
 sys.setrecursionlimit(10**8)
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-n = int(input())
-chu = list(map(int, input().split()))
+n=int(input())
 
-b = int(input())
-balls = list(map(int, input().split()))
+chu=list(map(int,input().split()))
 
-dp = [[False] * 15001 for _ in range(n + 1)]
+b=int(input())
 
-def calc(idx, left, right):
-    diff = abs(left - right)
-    
-    if diff > 15000 or dp[idx][diff]:
+balls=list(map(int,input().split()))
+maxVal=500*n+1
+dp=[[False] * (maxVal) for _ in range (n+1)]
+
+
+def calc(index,left,right):
+    diff=abs(left-right)
+    if dp[index][diff]:
         return
-    
-    dp[idx][diff] = True
-    
-    if idx == n:
+    dp[index][diff]=True
+    if index==n:
         return
-    
-    calc(idx + 1, left + chu[idx], right)
-    calc(idx + 1, left, right + chu[idx])
-    calc(idx + 1, left, right)
 
-calc(0, 0, 0)
+    calc(index+1,left+chu[index],right)
+    calc(index+1,left,right+chu[index])
+    calc(index+1,left,right)
 
-result = []
+
+
+calc(0,0,0)
 for ball in balls:
-    if ball > 15000:
-        result.append("N")
+    if ball<maxVal and dp[n][ball]:
+        print("Y",end=' ')
     else:
-        if dp[n][ball]:
-            result.append("Y")
-        else:
-            result.append("N")
+        print("N",end=' ')
 
-print(" ".join(result))
+print()
