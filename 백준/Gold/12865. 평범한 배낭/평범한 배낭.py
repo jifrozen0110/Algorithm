2cbeb6a6023f4d35d377df54e3b425f21c647a13
jifrozen0110 <answldjs1836@gmail.com>
2024-08-25 21:01:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 1556 ms, Memory: 33804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,8 +3,8 @@ input=sys.stdin.readline
 
 N,K=map(int,input().split())
 
-dp=[0]*(K+1)
-
+dp=[-1]*(K+1)
+dp[0]=0
 goods=[]
 
 for _ in range (N):
@@ -13,7 +13,7 @@ for _ in range (N):
 
 for w,v in goods:
     for i in range (K,-1,-1):
-        if i-w>=0:
-            dp[i]=max(dp[i],dp[i-w]+v)
+        if dp[i]!=-1 and i+w<=K:
+            dp[i+w]=max(dp[i+w],dp[i]+v)
 
-print(dp[K])
+print(max(dp))
