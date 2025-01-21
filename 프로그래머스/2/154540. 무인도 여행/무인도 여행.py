6593fa767ea95ca764af04da5db8fc9813e5fea0
jifrozen0110 <answldjs1836@gmail.com>
2025-01-21 15:52:03 +0900

    [level 2] Title: 무인도 여행, Time: 25.97 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,31 +1,30 @@
-import sys
-sys.setrecursionlimit(10000)
-
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+from collections import deque
 def solution(maps):
     answer = []
     visited=[[False]*len(maps[0]) for _ in range (len(maps))]
-    def dfs(maps,visited,x,y,value):
-        for dx,dy in [(1,0), (0,1), (-1,0), (0,-1)]:
-            nx=x+dx
-            ny=y+dy
-            if nx<0 or ny<0 or nx>=len(maps) or ny>=len(maps[0]):
-                continue
-            if visited[nx][ny]:
-                continue
-
-            if maps[nx][ny]=='X':
-                continue
-            visited[nx][ny]=True
-            value+=dfs(maps,visited,nx,ny,int(maps[nx][ny]))
-        return value
+    
     for i in range (len(maps)):
         for j in range (len(maps[0])):
-            if not visited[i][j] and maps[i][j]!='X':
-                visited[i][j]=True
-                answer.append(dfs(maps,visited,i,j,int(maps[i][j])))
-    
+            if maps[i][j]=='X' or visited[i][j]:
+                continue
+            
+            q=deque()
+            q.append((i,j))
+            visited[i][j]=True
+            value=int(maps[i][j])
+            while q:
+                x,y=q.popleft()
+                for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    nx=x+dx
+                    ny=y+dy
+                    if nx<0 or ny<0 or nx>=len(maps) or ny>=len(maps[0]):
+                        continue
+                    if maps[nx][ny]=='X' or visited[nx][ny]:
+                        continue
+                    visited[nx][ny]=True
+                    value+=int(maps[nx][ny])
+                    q.append((nx,ny))
+            answer.append(value)
     answer.sort()
     return answer if answer else [-1]
 
