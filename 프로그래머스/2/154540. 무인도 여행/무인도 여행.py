36395c671fb43cafa7328b3af808600764af009b
jifrozen0110 <answldjs1836@gmail.com>
2025-01-21 15:33:22 +0900

    [level 2] Title: 무인도 여행, Time: 13.92 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -6,6 +6,20 @@ dy=[0,0,-1,1]
 def solution(maps):
     answer = []
     visited=[[False]*len(maps[0]) for _ in range (len(maps))]
+    def dfs(maps,visited,x,y,value):
+        for dx,dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+            nx=x+dx
+            ny=y+dy
+            if nx<0 or ny<0 or nx>=len(maps) or ny>=len(maps[0]):
+                continue
+            if visited[nx][ny]:
+                continue
+
+            if maps[nx][ny]=='X':
+                continue
+            visited[nx][ny]=True
+            value+=dfs(maps,visited,nx,ny,int(maps[nx][ny]))
+        return value
     for i in range (len(maps)):
         for j in range (len(maps[0])):
             if not visited[i][j] and maps[i][j]!='X':
@@ -15,19 +29,6 @@ def solution(maps):
     answer.sort()
     return answer if answer else [-1]
 
-def dfs(maps,visited,x,y,value):
-    for i in range (4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if nx<0 or ny<0 or nx>=len(maps) or ny>=len(maps[0]):
-            continue
-        if visited[nx][ny]:
-            continue
-            
-        if maps[nx][ny]=='X':
-            continue
-        visited[nx][ny]=True
-        value+=dfs(maps,visited,nx,ny,int(maps[nx][ny]))
-    return value
+
         
     
