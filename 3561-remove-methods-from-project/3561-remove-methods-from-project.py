2fa8c1b571267d3c8533984b7fe92123d37429f5
jifrozen0110 <answldjs1836@gmail.com>
2025-01-24 14:46:36 +0900

    Time: 367 ms (67.37%), Space: 108 MB (27.12%) - LeetHub

---
+++ 3561-remove-methods-from-project/3561-remove-methods-from-project.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution:
+    def remainingMethods(
+        self, n: int, k: int, invocations: List[List[int]]
+    ) -> List[int]:
+        graph = defaultdict(list)
+        degree = [0] * n
+        for start, end in invocations:
+            graph[start].append(end)
+            degree[end] += 1
+        answer = [i for i in range(n)]
+        visited = set()
+        queue = deque()
+        queue.append(k)
+        while queue:
+            pos = queue.popleft()
+            if pos in visited:
+                continue
+            visited.add(pos)
+            for next in graph[pos]:
+                degree[next] -= 1
+                queue.append(next)
+        print(visited)
+
+        if all(degree[v] == 0 for v in visited):
+            return list(set(answer)-visited)
+        return answer
