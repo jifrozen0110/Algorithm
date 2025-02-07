49f7fee23a6fce2359201c245d5b136cc9daae1e
jifrozen0110 <answldjs1836@gmail.com>
2025-02-07 16:55:42 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,25 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {};
-        
-        HashSet<String> set=new HashSet<>();
-        
-        int index=0;
-        char last=words[0].charAt(0);
-        for(String word:words){
-            if(set.contains(word)){
-                break;
-            }
-            if(word.length()==1) break;
-            if(last!=word.charAt(0)) break;
-            last=word.charAt(word.length()-1);
-            set.add(word);
-            index++;
-        }
-        
+        int[] answer = new int[2];
 
-        if(set.size()==words.length) return new int[]{0,0};
-        return new int[]{index%n+1,index/n+1};
+        
+        char lastWord=words[0].charAt(0);
+        
+        HashSet<String> ms=new HashSet<String>();
+    
+        for (int i=0;i<words.length;i++){
+            if (words[i].length()<=1){
+                answer[0]=(i)%n+1;
+                answer[1]=((i)/n)+1;
+                return answer;
+            }
+            if (ms.contains(words[i])){
+                answer[0]=(i)%n+1;
+                answer[1]=((i)/n)+1;
+                return answer;
+            }
+            if (words[i].charAt(0)!=lastWord){
+                answer[0]=(i)%n+1;
+                answer[1]=((i)/n)+1;
+                return answer;
+            }
+            ms.add(words[i]);
+            lastWord=words[i].charAt(words[i].length()-1);
+        }
+
+        return answer;
     }
 }
