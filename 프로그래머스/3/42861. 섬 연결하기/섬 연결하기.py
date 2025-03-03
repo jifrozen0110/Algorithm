6d5f183a21602112835fad1a3f4738cced817999
jifrozen0110 <answldjs1836@gmail.com>
2025-03-03 16:08:45 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,27 +1,21 @@
-import heapq
+def find(node,parents):
+    if parents[node]==node:
+        return node
+    return find(parents[node],parents)
 
 def solution(n, costs):
     answer = 0
-    visited=[False]*n
     
-    graph={i : [] for i in range (n)}
-    start=-1
-    minValue=1e9
+    parents=[i for i in range (n)]
     
-    for cost in costs:
-        graph[cost[0]].append((cost[1],cost[2]))
-        graph[cost[1]].append((cost[0],cost[2]))
-
-    min_heap=[(0,0)]
-    
-    while min_heap:
-        cost,u=heapq.heappop(min_heap)
-        if visited[u]:
-            continue
-        visited[u]=True
-        answer+=cost
-        for v,cost in graph[u]:
-            if not visited[v]:
-                heapq.heappush(min_heap,(cost,v))
-    
+    edges=sorted([(cost[2],cost[0],cost[1]) for cost in costs])
+    cnt=0
+    for edge in edges:
+        if find(edge[1],parents)!=find(edge[2],parents):
+            parents[find(edge[2],parents)]=edge[1]
+            answer+=edge[0]
+            cnt+=1
+        if cnt==n-1:
+            break;
+        
     return answer
