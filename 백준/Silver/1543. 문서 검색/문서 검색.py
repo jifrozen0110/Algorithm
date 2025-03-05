203b41baba17bfc7db0bee29537c42e6e6e8032b
jifrozen0110 <answldjs1836@gmail.com>
2025-03-05 16:55:32 +0900

    [Silver V] Title: 문서 검색, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -6,17 +6,11 @@ str=input().strip()
 word=input().strip()
 answer=0
 index=0
-while index+len(word)-1<len(str):
-    isWrong=False
-    for j in range (len(word)):
-        if str[index+j]!=word[j]:
-            isWrong=True
-            break
-    if not isWrong:
-        index=index+len(word)
+while index<len(str):
+    if str[index:index+len(word)]==word:
         answer+=1
+        index+=len(word)
     else:
         index+=1
-
 
 print(answer)
