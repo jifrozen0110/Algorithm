0fe7d2a554f9a2599ae320d3a659bd055efe6ddc
jifrozen0110 <answldjs1836@gmail.com>
2025-04-21 12:32:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.82 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,30 +1,29 @@
+uf = []
+
+def find(a):
+    global uf
+    if uf[a] < 0: return a
+    uf[a] = find(uf[a])
+    return uf[a]
+
+def merge(a, b):
+    global uf
+    pa = find(a)
+    pb = find(b)
+    if pa == pb: return
+    uf[pa] += uf[pb]
+    uf[pb] = pa
+
 def solution(n, wires):
-    answer = n
-
-    graph = [[] for _ in range(n + 1)]
-    for a, b in wires:
-        graph[a].append(b)
-        graph[b].append(a)
-
-    def DFS(node, visited):
-        count=1
-        visited[node]=True
-        for n in graph[node]:
-            if not visited[n]:
-                count+=DFS(n,visited)
-        return count
-
-    for a, b in wires:
-        graph[a].remove(b)
-        graph[b].remove(a)
-
-        visited = [False] * (n + 1)
-        count = DFS(a, visited)
-
-        diff = abs((n - count) - count)
-        answer = min(answer, diff)
-
-        graph[a].append(b)
-        graph[b].append(a)
+    global uf
+    answer = int(1e9)
+    k = len(wires)
+    for i in range(k):
+        uf = [-1 for _ in range(n+1)]
+        tmp = [wires[x] for x in range(k) if x != i]
+        for a, b in tmp: merge(a, b)
+        
+        v = [x for x in uf[1:] if x < 0]
+        answer = min(answer, abs(v[0]-v[1]))
 
     return answer
